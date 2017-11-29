95c6f66161aa7ac1ff5ae49386cf235fc6d0a19c
Angie Siu <asiu@mscgroupny.com>
2017-11-29 10:29:17 -0500

    -init commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function App(props) {
+	return (
+		<h1>Hello world!</h1>	
+	);
+}
+
+
+// =============================
+
+ReactDOM.render(<App />, document.getElementById('root'));
